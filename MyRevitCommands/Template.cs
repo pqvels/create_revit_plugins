01935ff2995c9259c820197212d9682d54776049
pc <pc@DESKTOP-3R6MD9A>
2023-01-12 12:07:57 +0300

    Add project files.

---
+++ MyRevitCommands/Template.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+
+namespace MyRevitCommands
+{
+    [TransactionAttribute(TransactionMode.ReadOnly)]
+    public class Name : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+
+            var uiapp = commandData.Application;
+            var uidoc = uiapp.ActiveUIDocument;
+            var doc = uidoc.Document;
+
+            //Mainn code
+
+
+
+
+            return Result.Succeeded;
+        }
+    }
+}
